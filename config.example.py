ac3565f8867d3948692dba6b1ad614a2f2e909bc
SartoRiccardo <rikki.sarto@gmail.com>
2023-02-21 13:50:55 +0100

    Added co-owner support

--- config.example.py
+++ config.example.py
@@ -5,3 +5,7 @@ DB_USER = "postgres"
 DB_PSWD = "postgres"
 DB_HOST = "127.0.0.1"
 DB_NAME = "ct_ticket_tracker"
+
+CO_OWNER_IDS = [
+
+]
--- ct_ticket_tracker/cogs/OwnerCog.py
+++ ct_ticket_tracker/cogs/OwnerCog.py
@@ -1,7 +1,8 @@
 import discord
 import importlib
 from discord.ext import commands
-from typing import Optional, Literal
+from typing import Optional, Literal, Any
+import config
 
 
 SUCCESS_REACTION = '\N{THUMBS UP SIGN}'
@@ -9,66 +10,73 @@ SUCCESS_REACTION = '\N{THUMBS UP SIGN}'
 
 class OwnerCog(commands.Cog):
     ERROR_MESSAGE = "**ERROR:** {} - {}"
+    COG_PATH_TEMPLATE = "ct_ticket_tracker.cogs.{}Cog"
 
     def __init__(self, bot):
         self.bot = bot
 
-    def cog_unload(self):
+    def cog_unload(self) -> None:
         pass
 
+    @staticmethod
+    def is_owner():
+        async def predicate(ctx: discord.ext.commands.Context):
+            return ctx.author.id in config.CO_OWNER_IDS or await ctx.bot.is_owner(ctx.author)
+        return discord.ext.commands.check(predicate)
+
     @commands.command()
-    @commands.is_owner()
-    async def sync(self, ctx, where: Optional[Literal["."]] = None):
+    @is_owner()
+    async def sync(self, ctx: discord.ext.commands.Context, where: Optional[Literal["."]] = None) -> None:
         if where == ".":
             synced = await ctx.bot.tree.sync(guild=ctx.guild)
         else:
             synced = await ctx.bot.tree.sync()
         await ctx.send(f"Synced {len(synced)} commands ({'globally' if where is None else 'here'}).")
 
-    @commands.group()
-    @commands.is_owner()
-    async def cog(self, ctx):
+    @commands.group(aliases=["cogs"])
+    @is_owner()
+    async def cog(self, ctx: discord.ext.commands.Context) -> None:
         if ctx.invoked_subcommand is None:
-            pass
+            cogs = [str_cog for str_cog in self.bot.cogs]
+            await ctx.send("Loaded cogs: `" + "`, `".join(cogs) + "`")
 
     @cog.command(aliases=["add"])
-    @commands.is_owner()
-    async def load(self, ctx, name):
+    @is_owner()
+    async def load(self, ctx: discord.ext.commands.Context, name: str) -> None:
         try:
-            await self.bot.load_extension(f"ct_ticket_tracker.cogs.{name}Cog")
+            name = name[0].upper() + name[1:]
+            cog_name = OwnerCog.COG_PATH_TEMPLATE.format(name)
+            await self.bot.load_extension(cog_name)
             await ctx.message.add_reaction(SUCCESS_REACTION)
         except Exception as e:
             await ctx.send(self.ERROR_MESSAGE.format(type(e).__name__, e))
 
     @cog.command(aliases=["remove"])
-    @commands.is_owner()
-    async def unload(self, ctx, name):
+    @is_owner()
+    async def unload(self, ctx: discord.ext.commands.Context, name: str) -> None:
         try:
-            if f"ct_ticket_tracker.cogs.{name}Cog" != __name__:
-                await self.bot.unload_extension(f"ct_ticket_tracker.cogs.{name}Cog")
+            name = name[0].upper() + name[1:]
+            cog_name = OwnerCog.COG_PATH_TEMPLATE.format(name)
+            if cog_name != __name__:
+                await self.bot.unload_extension(cog_name)
                 await ctx.message.add_reaction(SUCCESS_REACTION)
             else:
                 await ctx.send(
-                    f"You cannot unload the `{name}` cog. Did you mean `,cog reload {name}`?"
+                    f"You cannot unload the `{name}` cog. Did you mean `cog reload {name}`?"
                 )
         except Exception as e:
             await ctx.send(self.ERROR_MESSAGE.format(type(e).__name__, e))
 
     @cog.command()
-    @commands.is_owner()
-    async def reload(self, ctx, name):
+    @is_owner()
+    async def reload(self, ctx: discord.ext.commands.Context, name: str) -> None:
         try:
-            await self.bot.unload_extension(f"ct_ticket_tracker.cogs.{name}Cog")
-            await self.bot.load_extension(f"ct_ticket_tracker.cogs.{name}Cog")
+            name = name[0].upper() + name[1:]
+            cog_name = OwnerCog.COG_PATH_TEMPLATE.format(name)
+            await self.bot.reload_extension(cog_name)
             await ctx.message.add_reaction(SUCCESS_REACTION)
         except Exception as e:
             await ctx.send(self.ERROR_MESSAGE.format(type(e).__name__, e))
-
-    @cog.command(alias=["list"])
-    @commands.is_owner()
-    async def list(self, ctx):
-        cogs = [str_cog for str_cog in self.bot.cogs]
-        await ctx.send("Loaded cogs: " + ", ".join(cogs))
 
 
 async def setup(bot: commands.Bot) -> None:
