f8299f871a26effe7ed22b3e54a9442ceeb2436e
SartoRiccardo <rikki.sarto@gmail.com>
2023-03-28 00:48:50 +0200

    Error Handler Cog interaction fix

--- ct_ticket_tracker/classes/ErrorHandlerCog.py
+++ ct_ticket_tracker/classes/ErrorHandlerCog.py
@@ -1,4 +1,5 @@
 import discord
+import traceback
 from discord.ext import commands
 from ct_ticket_tracker.exceptions import WrongChannelMention
 
@@ -6,19 +7,18 @@ from ct_ticket_tracker.exceptions import WrongChannelMention
 class ErrorHandlerCog(commands.Cog):
     async def cog_app_command_error(self, interaction: discord.Interaction,
                                     error: discord.app_commands.AppCommandError) -> None:
+        print(error)
+
+        content = "An error has occurred!"
         error_type = type(error.__cause__)
         if error_type == WrongChannelMention:
-            await interaction.response.send_message(
-                "You did not provide a valid channel! Please *mention* the channel, don't just type its name.\n"
-                f"For example, instead of \"{interaction.channel.name}\" it should be \"<#{interaction.channel_id}>\"",
-                ephemeral=True
-            )
+            content = "You did not provide a valid channel! Please *mention* the channel, don't just type its name.\n" + \
+                      f"For example, instead of \"{interaction.channel.name}\" it should be \"<#{interaction.channel_id}>\""
         elif error_type == discord.app_commands.errors.MissingPermissions:
-            await interaction.response.send_message(
-                "You don't have the perms to execute this command. Sorry!",
-                ephemeral=True
-            )
+            content = "You don't have the perms to execute this command. Sorry!"
+
+        if interaction.response.is_done():
+            await interaction.edit_original_response(content=content)
         else:
-            await interaction.response.send_message(
-                "An error has occurred!", ephemeral=True
-            )
+            await interaction.response.send_message(content, ephemeral=True)
+
