7ace60833c2790ba02d7a3667eb83aeba94db51d
SartoRiccardo <rikki.sarto@gmail.com>
2023-03-28 00:57:09 +0200

    Error Handler Cog interaction fix

--- ct_ticket_tracker/classes/ErrorHandlerCog.py
+++ ct_ticket_tracker/classes/ErrorHandlerCog.py
@@ -1,5 +1,4 @@
 import discord
-import traceback
 from discord.ext import commands
 from ct_ticket_tracker.exceptions import WrongChannelMention
 
@@ -7,10 +6,13 @@ from ct_ticket_tracker.exceptions import WrongChannelMention
 class ErrorHandlerCog(commands.Cog):
     async def cog_app_command_error(self, interaction: discord.Interaction,
                                     error: discord.app_commands.AppCommandError) -> None:
-        print(error)
+        print(error, type(error.__cause__), type(error))
 
         content = "An error has occurred!"
         error_type = type(error.__cause__)
+        if error.__cause__ is None:
+            error_type = type(error)
+
         if error_type == WrongChannelMention:
             content = "You did not provide a valid channel! Please *mention* the channel, don't just type its name.\n" + \
                       f"For example, instead of \"{interaction.channel.name}\" it should be \"<#{interaction.channel_id}>\""
