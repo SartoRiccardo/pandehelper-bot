84dbeaca03831a5b3caf2590b2f287f0d5a167d9
SartoRiccardo <rikki.sarto@gmail.com>
2023-04-11 23:54:40 +0200

    Added /mintime

--- ct_ticket_tracker/cogs/UtilsCog.py
+++ ct_ticket_tracker/cogs/UtilsCog.py
@@ -1,6 +1,7 @@
 import asyncio
 import ct_ticket_tracker.utils.io
 import discord
+from datetime import timedelta
 from discord.ext import commands
 from ct_ticket_tracker.classes import ErrorHandlerCog
 
@@ -8,6 +9,7 @@ from ct_ticket_tracker.classes import ErrorHandlerCog
 class UtilsCog(ErrorHandlerCog):
     help_descriptions = {
         "longestround": "Gives you info about a race's longest round, and the rounds that follow.",
+        "mintime": "Tells you what time you'll get if you pclean a race after fullsending on a certain round.",
         "tag": "Sends a pre-written message associated to a tag. Usually for FAQs.\n"
                "Type the command with no parameters to see all available tags.",
         "github": "Get a link to the bot's repo. It's open source!"
@@ -49,6 +51,36 @@ class UtilsCog(ErrorHandlerCog):
                 f"The last bloon should be a **{round_checkpoints[0]['last_bloon']}** " \
                 f"(or a **{round_checkpoints[0]['last_bloon_reverse']}** if the race is on Reverse)."
         await interaction.response.send_message(reply)
+
+    @discord.app_commands.command(name="mintime",
+                                  description="Calculate the min time you can get on races when you fullsend.")
+    @discord.app_commands.describe(from_round="The round you're fullsending from.",
+                                   to_round="The last round of the race.",
+                                   send_time_formatted="The time you fullsend at (e.g. 0:50). "
+                                                       "Don't include milliseconds.")
+    @discord.app_commands.rename(send_time_formatted="send_time")
+    async def mintime(self, interaction: discord.Interaction, from_round: int,
+                      to_round: int, send_time_formatted: str) -> None:
+        if send_time_formatted.isnumeric():
+            send_time = int(send_time_formatted)
+        else:
+            minutes, seconds = send_time_formatted.split(":")
+            send_time = int(minutes)*60 + int(seconds)
+
+        rounds = await asyncio.to_thread(ct_ticket_tracker.utils.io.get_race_rounds)
+        longest = sorted(rounds[from_round-1:to_round], key=lambda x: x["length"])[-1]
+        send_delay = (longest["round"]-from_round)*0.2
+        final_time = send_time + longest["length"] + send_delay
+        minutes = int(final_time/60)
+
+        message = f"The longest round {from_round}-{to_round} is **R{longest['round']}** *({longest['length']:.2f}s)*\n" \
+                  f"With the round send delay, to get there in {send_delay:.2f}s\n\n" \
+                  f"**Min time: {minutes}:{final_time - minutes * 60:05.2f}** *(if fullsent from {send_time}s)*.\n"
+
+        # message = f"If you are at R{from_round} and you fullsend at {send_time_formatted}, the longest round is " \
+        #           f"R{longest['round']} ({longest['length']:.2f}s), plus round send delay, if you pclean you'll get " \
+        #           f"**{minutes}:{final_time-minutes*60:.2f}**."
+        await interaction.response.send_message(message)
 
     @discord.app_commands.command(name="help",
                                   description="Get info about the bot's commands.")
