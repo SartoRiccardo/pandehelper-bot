ea762ed89f40e12865922fdc05cab78cde5ac818
SartoRiccardo <rikki.sarto@gmail.com>
2023-04-17 16:01:59 +0200

    Raid log revamp

--- bot/cogs/RaidLogCog.py
+++ bot/cogs/RaidLogCog.py
@@ -1,100 +1,235 @@
 import discord
 from discord.ext import commands
+import bot.db.queries
 from bot.classes import ErrorHandlerCog
+from bot.exceptions import WrongChannelMention, MustBeForum
 import bot.utils.bloons
 import re
+import bloonspy
+from typing import List
+
+
+thread_init_message = """
+# How to make a good post
+
+Other than posting the general strategy (e.g. "Sniper 025", "Gwen Solo", ...) there are other things you can say to make your report better:
+* __Post the score you got.__ For Least Tiers/Least Cash, post the exact score, for Race and Boss tiles you can post an approximate time (e.g. "Sub 1:30").
+* __Post the relics you used.__ It's extremely important you show what you used since some strategies are only possible with them.
+* If you use Powers or Abilities, __describe the activation timing on important rounds.__ For example: "Place a Glue Trap on R63" or "Use Phoenix when you start getting overwhealmed on R77" or "Use both SMS on Tier 1" etc...
+
+There are also extra rules specific to tile types.
+
+## Least Tiers/Least Cash
+* __Post a screenshot of the map once you win.__ Knowing tower placements can be really important!
+
+## Boss
+* __Describe the farming methods you used.__ Bloontrap? Buy farm on Round 5? Geraldo? You tell me!
+* __Post a screenshot of your setup for every tier.__
+
+## Race
+You can do 2 things for race tiles, which one is up to you:
+* __Post a video of your race.__ That's the simplest way to do it although kind of annoying to set up.
+* __Accurately describe what to get and which rounds to send.__ For example:
+ * Buy Ace 020, send R20
+ * Ace 020 > 030, send R39
+ * Buy Bomb 003 > 203 > 204
+ * Buy another Ace 030, send R49
+ * Buy another Ace 030, spam 204 Bomb.
+ * Fullsend
+ * Ace 030 > 040, activate, sell for 205 Sniper for clean."""[1:]
 
 
 class RaidLogCog(ErrorHandlerCog):
+    help_descriptions = {
+        None: "Manages a forum to post tile strategies",
+        "tilestratforum": {
+            "create": "Creates a new Tile Strat forum. You can rename it & set the perms to whatever you want",
+            "set": "Sets an existing channel as the Tile Strat forum. Using [[create]] is recommended though.",
+            "unset": "Makes the bot stop tracking the current Tile Strat forum.",
+        },
+        "tilestrat": "Looks up a strat in the Tile Strat forum.",
+    }
+
+    group_tilestratchannel = discord.app_commands.Group(
+        name="tilestratchannel",
+        description="Manage the Tile Strat forum."
+    )
+
     def __init__(self, bot: commands.Bot) -> None:
         super().__init__(bot)
 
-    @discord.app_commands.command(name="raidlog", description="Get a Raid Log thread")
+    @discord.app_commands.command(name="tilestrat", description="Get the strat thread for a tile")
     @discord.app_commands.describe(tile_code="The tile code to look up.")
     @discord.app_commands.guild_only()
-    async def cmd_raid_log_thread(self, interaction: discord.Interaction, tile_code: str) -> None:
-        forum_id = 1049000279099588719
+    async def cmd_search(self, interaction: discord.Interaction, tile_code: str) -> None:
+        forum_id = await bot.db.queries.get_tile_strat_forum(interaction.guild_id)
+        if forum_id is None:
+            await interaction.response.send_message(
+                "You don't have a Tile Strats forum set! Run /tilestratforum create or /tilestratforum set to have one.",
+                ephemeral=True
+            )
+            return
 
-        tile_re = r"[A-GM][A-GR][A-HX]"
-        tile_code = tile_code.upper()
-        if re.search(tile_re, tile_code) is None:
-            await interaction.response.send_message(f"The tile {tile_code} doesn't exist!",
-                                                    ephemeral=True)
+        tile_code = tile_code.strip().upper()
+        tile_re = r"(?:M|[A-G])(?:R|[A-G])(?:X|[A-H])"
+        if len(tile_code) != 3 or re.match(tile_re, tile_code) is None:
+            await interaction.response.send_message(
+                f"The tile {tile_code} doesn't exist!",
+                ephemeral=True
+            )
             return
 
         await interaction.response.defer()
+
         try:
             forum_channel = await interaction.guild.fetch_channel(forum_id)
-        except (discord.errors.Forbidden, discord.errors.InvalidData):
-            await interaction.edit_original_response(content=f"This command is only for the Pandemonium server. Sorry!")
+        except discord.errors.Forbidden:
+            await interaction.edit_original_response(
+                content=f"I don't have permission to see that channel!"
+            )
+            return
+        except discord.errors.InvalidData:
+            await interaction.edit_original_response(
+                content=f"That channel isn't even in this server!"
+            )
             return
 
+        current_event_tag = f"Season {bot.utils.bloons.get_current_ct_number()}"
+        if current_event_tag not in [tag.name for tag in forum_channel.available_tags]:
+            await forum_channel.create_tag(name=current_event_tag)
+
+        old_strats = []
+        found = None
         for thread in forum_channel.threads:
             if tile_code in thread.name.upper():
-                thread_url = f"https://discord.com/channels/{interaction.guild.id}/{thread.id}"
-                embed = discord.Embed(title=f"Raid Log - Tile {tile_code}",
-                                      colour=discord.Color.orange(),
-                                      description=f"Link to the thread: <#{thread.id}>\n"
-                                                  f"If it doesn't work, click [here]({thread_url}) "
-                                                  f"to jump to that thread!")
-                await interaction.edit_original_response(embed=embed)
-                return
+                if current_event_tag in [tag.name for tag in thread.applied_tags]:
+                    found = thread
+                    await interaction.edit_original_response(
+                        embed=self.get_raidlog_embed(thread, old_strats, loading=True)
+                    )
+                else:
+                    old_strats.append(thread)
 
-        async for thread in forum_channel.archived_threads(limit=200):
+        async for thread in forum_channel.archived_threads(limit=None):
             if tile_code in thread.name.upper():
-                thread_url = f"https://discord.com/channels/{interaction.guild.id}/{thread.id}"
-                embed = discord.Embed(title=f"Raid Log - Tile {tile_code}",
-                                      colour=discord.Color.orange(),
-                                      description=f"Link to the thread: <#{thread.id}>\n"
-                                                  f"If it doesn't work, click [here]({thread_url})"
-                                                  f"to jump to that thread!")
-                await interaction.edit_original_response(embed=embed)
-                return
+                if current_event_tag in [tag.name for tag in thread.applied_tags]:
+                    found = thread
+                    await interaction.edit_original_response(
+                        embed=self.get_raidlog_embed(thread, old_strats, loading=True)
+                    )
+                else:
+                    old_strats.append(thread)
 
-        await interaction.edit_original_response(content=f"There is no thread named {tile_code}!")
+        if not found:
+            # tile_info = bloonspy.get_tile_info_somehow(tile_code)
 
-    @discord.app_commands.command(name="newraidlog", description="Mark the beginning of a new event in Raid Log")
-    @discord.app_commands.checks.has_permissions(manage_guild=True)
-    @discord.app_commands.guild_only()
-    async def cmd_raidlog_reset(self, interaction: discord.Interaction) -> None:
-        forum_id = 1049000279099588719
-        tile_re = r"[A-GM][A-GR][A-HX]"
+            tags = [
+                discord.utils.get(forum_channel.available_tags, name=current_event_tag)
+            ]
+            thread_template = "[{map}] {tile_code}"
+            found = await forum_channel.create_thread(
+                name=thread_template.format(map="", tile_code=tile_code),
+                content=thread_init_message,
+                applied_tags=tags,
+            )
 
-        await interaction.response.send_message(
-            "Announcing a new season in Raid Log! It might take a while...",
-            ephemeral=True
+        await interaction.edit_original_response(
+            embed=self.get_raidlog_embed(found, old_strats)
         )
 
-        try:
-            forum_channel = discord.utils.get(interaction.guild.channels, id=forum_id)
-            if forum_channel is None:
-                forum_channel = await interaction.guild.fetch_channel(forum_id)
-        except (discord.errors.Forbidden, discord.errors.InvalidData):
-            await interaction.edit_original_response(content=f"This command is only for the Pandemonium server. Sorry!")
-            return
+    @group_tilestratchannel.command(name="create", description="Create a Tile Strats forum.")
+    @discord.app_commands.checks.has_permissions(manage_guild=True)
+    @discord.app_commands.guild_only()
+    async def cmd_create_raidlog(self, interaction: discord.Interaction) -> None:
+        forum = await interaction.guild.create_forum("tile-strats")
 
-        for thread in forum_channel.threads:
-            if re.match(tile_re, thread.name.upper()) is not None:
-                await self.announce_ct_season_raidlog(thread)
+        await bot.db.queries.set_tile_strat_forum(interaction.guild_id, forum.id)
+        await interaction.response.send_message(
+            f"Done! <#{forum.id}> is now your Tile Strats forum!"
+        )
 
-        async for thread in forum_channel.archived_threads(limit=200):
-            if re.match(tile_re, thread.name.upper()) is not None:
-                await self.announce_ct_season_raidlog(thread)
+    @group_tilestratchannel.command(
+        name="set",
+        description="Set an existing channel as the server's Tile Strats forum"
+    )
+    @discord.app_commands.describe(channel="The forum to set as the server's Tile Strats forum")
+    @discord.app_commands.checks.has_permissions(manage_guild=True)
+    @discord.app_commands.guild_only()
+    async def cmd_set_raidlog(self, interaction: discord.Interaction, channel: str) -> None:
+        channel = channel.strip()
+        if len(channel) <= 3 or not channel[2:-1].isnumeric():
+            raise WrongChannelMention()
+        channel_id = int(channel[2:-1])
+        print(channel_id)
+        forum = discord.utils.get(interaction.guild.forums, id=channel_id)
+        if not forum:
+            raise WrongChannelMention()
+        if not isinstance(forum, discord.ForumChannel):
+            raise MustBeForum()
 
-        await interaction.edit_original_response(content="All done!")
+        await bot.db.queries.set_tile_strat_forum(interaction.guild_id, channel_id)
+        await interaction.response.send_message(
+            f"Done! <#{channel_id}> is now your Tile Strats forum!"
+        )
 
-    async def announce_ct_season_raidlog(self, channel: discord.Thread) -> None:
-        ct_num = bot.utils.bloons.get_current_ct_number(breakpoint_on_event_start=False)
-        season_message = f"__**— CONTESTED TERRITORY {ct_num} —**__"
+    @group_tilestratchannel.command(name="unset", description="Stop tracking the current Tile Strats forum")
+    @discord.app_commands.checks.has_permissions(manage_guild=True)
+    @discord.app_commands.guild_only()
+    async def cmd_unset_raidlog(self, interaction: discord.Interaction) -> None:
+        await bot.db.queries.del_tile_strat_forum(interaction.guild_id)
+        await interaction.response.send_message(
+            "Done! You server no longer has a tile strat forum!"
+        )
 
-        last_message = channel.last_message
-        if last_message is None:
-            last_message = [msg async for msg in channel.history(limit=1)][0]
+    @staticmethod
+    def get_raidlog_embed(
+            thread: discord.Thread,
+            old_threads: List[discord.Thread],
+            loading: bool = False
+    ) -> discord.Embed:
+        content = f"Click [HERE]({RaidLogCog.get_channel_url(thread)}) to go jump to the thread!"
 
-        if last_message.author == self.bot.user:
-            await last_message.edit(content=season_message)
-        else:
-            await channel.send(season_message)
+        if len(old_threads) > 0:
+            threads_str = []
+            thread_template = "• [CT{ct_num}]({thread_url}) - {tile_type} {boss_type}"
+            tile_types = ["Least Cash", "Least Tiers", "Race", "Boss"]
+            bosses = ["Vortex", "Lych", "Bloonarius"]
+            for old_thr in old_threads:
+                ct_num = "???"
+                tile_type = "Unknown Tile type"
+                boss_type = ""
+                for tag in old_thr.applied_tags:
+                    if "Event" in tag.name:
+                        ct_num = tag.name[-2:]
+                    elif tag.name in tile_types:
+                        tile_type = tag.name
+                        if tag.emoji:
+                            tile_type = f"<:{tag.emoji.name}:{tag.emoji.id}> {tile_type}"
+                    elif tag.name in bosses:
+                        boss_type = tag.name
+                        if tag.emoji:
+                            boss_type = f"<:{tag.emoji.name}:{tag.emoji.id}> {boss_type}"
+
+                threads_str.append(thread_template.format(
+                    ct_num=ct_num,
+                    thread_url=RaidLogCog.get_channel_url(old_thr),
+                    tile_type=tile_type,
+                    boss_type=boss_type
+                ))
+            content += "\n\n" + "\n".join(threads_str)
+
+        if loading:
+            content += "\n\nChecking for past threads for this tile code..."
+
+        return discord.Embed(
+            title=thread.name,
+            description=content,
+            color=discord.Color.orange()
+        )
+
+    @staticmethod
+    def get_channel_url(channel: discord.abc.MessageableChannel):
+        return f"https://discord.com/channels/{channel.guild.id}/{channel.id}"
 
 
 async def setup(bot: commands.Bot) -> None:
