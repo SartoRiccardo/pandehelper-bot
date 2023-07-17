ca80903460df8d9db9921d46c79892e06e80cbe3
SartoRiccardo <rikki.sarto@gmail.com>
2023-07-17 18:42:33 +0200

    Times two

--- bot/utils/bloons.py
+++ bot/utils/bloons.py
@@ -69,7 +69,7 @@ def get_current_ct_number(breakpoint_on_event_start: bool = True) -> int:
 
 def get_current_ct_period() -> Tuple[datetime.datetime, datetime.datetime]:
     current = get_current_ct_number()
-    start = FIRST_CT_START + datetime.timedelta(days=EVENT_DURATION*current)
+    start = FIRST_CT_START + datetime.timedelta(days=EVENT_DURATION*current*2)
     return start, start+datetime.timedelta(days=EVENT_DURATION)
 
 
