af5ebdce16d9dedc19c50b1d1a22599ef46bbdfd
SartoRiccardo <rikki.sarto@gmail.com>
2023-10-22 14:43:10 +0200

    feat: Prettier buttons for /spawnlock

--- bot/views/SpawnlockPaginate.py
+++ bot/views/SpawnlockPaginate.py
@@ -4,12 +4,12 @@ import bot.utils.bloons
 
 
 class TileButton(discord.ui.Button):
-    def __init__(self, tile_name: str, idx: int, sup_callback: Callable):
+    def __init__(self, tile_name: str, idx: int, sup_callback: Callable, active: bool = True):
         self.idx = idx
         self.sup_callback = sup_callback
         super().__init__(
             label=tile_name,
-            style=discord.ButtonStyle.blurple
+            style=discord.ButtonStyle.blurple if active else discord.ButtonStyle.gray,
         )
 
     async def callback(self, interaction: discord.Interaction) -> Any:
@@ -21,14 +21,15 @@ class SpawnlockPaginateView(discord.ui.View):
     def __init__(self,
                  tile_data: list[dict[str, Any]],
                  original_interaction: discord.Interaction = None,
-                 timeout: float = 180):
+                 timeout: float = 180,
+                 current: int = 0):
         super().__init__(timeout=timeout)
         self.tile_data = tile_data
-        self.current = 0
+        self.current = current
         self.original_interaction = original_interaction
 
         for i in range(len(tile_data)):
-            self.add_item(TileButton(tile_data[i]["Code"], i, self.edit_embed))
+            self.add_item(TileButton(tile_data[i]["Code"], i, self.edit_embed, i == self.current))
 
     def set_original_interaction(self, original_interaction: discord.Interaction) -> None:
         self.original_interaction = original_interaction
@@ -36,7 +37,7 @@ class SpawnlockPaginateView(discord.ui.View):
     async def edit_embed(self, interaction: discord.Interaction, tile_idx: int) -> None:
         if self.original_interaction.user != interaction.user:
             await interaction.response.send_message(
-                content="💢 Hands off!",
+                content="💢 You can't click this embed! **Run the command yourself!**",
                 ephemeral=True,
             )
             return
@@ -48,7 +49,6 @@ class SpawnlockPaginateView(discord.ui.View):
             )
             return
 
-        self.current = tile_idx
         await interaction.response.send_message(
             content=f"ⓘ Showing tile **{self.tile_data[tile_idx]['Code']}**!",
             ephemeral=True,
@@ -57,4 +57,5 @@ class SpawnlockPaginateView(discord.ui.View):
         embed = bot.utils.bloons.raw_challenge_to_embed(self.tile_data[tile_idx])
         await self.original_interaction.edit_original_response(
             embed=embed,
+            view=SpawnlockPaginateView(self.tile_data, self.original_interaction, current=self.current),
         )
