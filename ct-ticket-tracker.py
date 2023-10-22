b900fb15ff9e2283e277621da3589593507c467a
SartoRiccardo <rikki.sarto@gmail.com>
2023-10-22 15:47:16 +0200

    misc: Rename RaidLog

--- ct-ticket-tracker.py
+++ ct-ticket-tracker.py
@@ -29,7 +29,7 @@ class CtTicketTracker(commands.Bot):
             "OwnerCog",
             "TrackerCog",
             "LeaderboardCog",
-            "RaidLogCog",
+            "TilestratCog",
             "UtilsCog",
             #"VerifyCog",
             "PlannerCog",
