b92dc161b5516f060dde679e95ec62ed67ccaed5
SartoRiccardo <rikki.sarto@gmail.com>
2024-09-27 13:14:12 +0200

    fix: Pools connection properly

--- bot/db/connection.py
+++ bot/db/connection.py
@@ -1,27 +1,33 @@
 import asyncpg
 import config
 from bot.utils.colors import red, purple
+from functools import wraps
 
-connection = None
+pool: asyncpg.Pool | None
 
 
 async def start():
-    global connection
+    global pool
     try:
-        connection = await asyncpg.create_pool(
+        pool = await asyncpg.create_pool(
             user=config.DB_USER, password=config.DB_PSWD,
             database=config.DB_NAME, host=config.DB_HOST
         )
-        print(f"{purple('[Postgres]')} Connected")
+        print(f"{purple('[PSQL]')} Connected")
     except:
-        print(f"{red('[Postgres]')} Error connecting")
+        print(f"{purple('[PSQL]')} {red('Error connecting to Postgres database')}")
 
 
-def postgres(func):
-    async def inner(*args, **kwargs):
-        if connection is None:
+def postgres(wrapped):
+    @wraps(wrapped)
+    async def wrapper(*args, **kwargs):
+        if "conn" in kwargs:
+            return await wrapped(*args, **kwargs)
+
+        if pool is None:
             return
-        return await func(*args, **kwargs, conn=connection)
-    return inner
+        async with pool.acquire() as conn:
+            return await wrapped(*args, **kwargs, conn=conn)
+    return wrapper
 
 
